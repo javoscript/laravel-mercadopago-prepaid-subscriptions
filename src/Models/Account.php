6dfd67ff6f3c3852361f5e080d0c2ddef096d249
Javoscript <javougarte@gmail.com>
2019-06-10 01:57:12 -0300

    👌 IMPROVE: Added convinience methods

--- src/Models/Account.php
+++ src/Models/Account.php
@@ -9,8 +9,8 @@ class Account extends Model
 {
     protected $table = 'prepaid_subs_accounts';
 
-    protected $dates = [
-        'expiration_date',
+    protected $casts = [
+        'expiration_date' => 'date:d-m-Y'
     ];
 
     protected $fillable = [
@@ -18,6 +18,12 @@ class Account extends Model
         'prepaid_subable_id',
         'prepaid_subable_type',
     ];
+
+    protected $appends = [
+        'subscription_status',
+        'payments'
+    ];
+
 
     public function prepaid_subable()
     {
@@ -27,5 +33,23 @@ class Account extends Model
     public function payments()
     {
         return $this->hasMany('Javoscript\PrepaidSubs\Models\Payment')->latest();
+    }
+
+
+    public function getSubscriptionStatusAttribute()
+    {
+        $successful_payments_count = $this->payments()->successful()->count();
+        if (\Carbon\Carbon::today() <= $this->expiration_date) {
+            if ($successful_payments_count == 0) {
+                return "free_trial";
+            }
+            return "active";
+        }
+        return "expired";
+    }
+
+    public function getPaymentsAttribute()
+    {
+        return $this->payments()->get();
     }
 }
